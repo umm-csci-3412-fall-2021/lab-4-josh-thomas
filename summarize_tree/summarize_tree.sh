2b1358a94c99f5f96c5c35deb2b211e2d727929e
Josh <quist>
2021-10-14 12:33:58 -0500

    Add some comments
    Added some comments to the shell script.

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# finds all directories and counts them
 variable=($(find $1 -maxdepth 16 -type d -print | wc -l | xargs))
 
+# finds all files and counts them
 variable2=($(find $1 -maxdepth 16 -type f -print | wc -l | xargs))
 
 echo "There were "$variable" directories."
